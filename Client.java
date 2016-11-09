364e74e66f361945db29a52aa4fe0ff120287854
Deborah Lewis <deborahlewis@users.noreply.github.com>
2016-11-09 10:41:33 -0500

    Learning about sockets

--- Client.java
+++ Client.java
@@ -1,14 +1,42 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *Learning about Sockets
  */
 package socketfun;
 
-/**
- *
- * @author Debbie
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
-    
-}
+
+    public static void main(String[] args) throws IOException {
+        
+        System.out.println("Start Client"); //testing
+        //create socket
+        Socket socket = new Socket("localhost", 67);
+
+        System.out.println("Open connection"); //testing
+        //create writer
+        PrintWriter out = new PrintWriter(socket.getOutputStream());
+        //receive text
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        
+        //send text
+        System.out.println("Send message"); //testing
+        out.println("Hello Server");
+        
+        //flush stream
+        out.flush();
+        
+        //read response from the server
+        System.out.println("Receive response"); //testing
+        String answer = in.readLine();
+        System.out.println("Server says: " + answer);
+        
+        //close socket
+        socket.close();
+
+    } //end main    
+} //end client
--- Server.java
+++ Server.java
@@ -1,24 +1,56 @@
 /*
- * Deborah Lewis
- * Basic Java Program to learn about Web Sockets
- * Demo from https://www.youtube.com/watch?v=6UfL9YW9z_Y&list=PLfkYWLGFGjwHkG-xTX_5gubwIf6S-wFRJ&index=2 
+ *Learning about Sockets
  */
 package socketfun;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import javax.swing.JFrame;
 
 public class Server {
 
-
     public static void main(String[] args) throws IOException {
         
-        ServerSocket ss = new ServerSocket(67); //create new server socket
-        Socket socket = ss.accept(); //listens to server request
+        //create server control GUI
+        JFrame window = new JFrame("server control");
+        window.setBounds(100, 50, 100, 50);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        System.out.println("Starting Up"); //testing
+        //create new server socket
+        ServerSocket ss = new ServerSocket(67);
+
+        while (true) {
+            //listener
+            Socket socket = ss.accept();
+
+            System.out.println("Got a Connection"); //testing
+            //receive text
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //create writer
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+            //read the text
+            String line = in.readLine(); //read one line
+            while (line != null) { //while the line isn't null
+                System.out.println(line); //display line
+                out.println("Thanks for your comments!"); //acknowlege receipt of message
+
+                //flush stream
+                out.flush();
+
+                line = in.readLine(); //read next line
+            }
+        } //end while  
         
-        
-    }
-    
-}
+        //close socket
+ //       ss.close();
+
+    } //end main
+
+} //end server class
